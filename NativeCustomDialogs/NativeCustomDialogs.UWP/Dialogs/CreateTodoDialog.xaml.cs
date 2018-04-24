2c1e727c433d17b17cec669933369751952b4a39
Damien <damientohin@gmail.com>
2018-04-24 11:51:27 +0100

    I added UWp Validation

--- NativeCustomDialogs/NativeCustomDialogs.UWP/Dialogs/CreateTodoDialog.xaml.cs
+++ NativeCustomDialogs/NativeCustomDialogs.UWP/Dialogs/CreateTodoDialog.xaml.cs
@@ -4,8 +4,10 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Windows.Input;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -21,19 +23,44 @@ namespace NativeCustomDialogs.UWP.Dialogs
     public sealed partial class CreateTodoDialog : ContentDialog
     {
         CreateTodoViewModel ViewModel => DataContext as CreateTodoViewModel;
+        bool _canClose;
 
         public CreateTodoDialog(CreateTodoViewModel vm)
         {
             this.InitializeComponent();
             DataContext = vm;
+            Closing += CreateCategoryDialog_Closing;
+        }
+
+        private void CreateCategoryDialog_Closing(ContentDialog sender, ContentDialogClosingEventArgs args)
+        {
+            if (!_canClose)
+            {
+                args.Cancel = true;
+            }
         }
 
         private void ContentDialog_PrimaryButtonClick(ContentDialog sender, ContentDialogButtonClickEventArgs args)
         {
+            if(string.IsNullOrEmpty(TitleDialog.Text))
+            {
+                TitleDialog.BorderBrush = new SolidColorBrush(Colors.Red);
+            }
+            else
+            {
+                if((ViewModel.CreateTodo as ICommand).CanExecute(null))
+                {
+                    (ViewModel.CreateTodo as ICommand).Execute(null);
+                }
+                _canClose = true;
+                this.Hide();
+            }
         }
 
         private void ContentDialog_SecondaryButtonClick(ContentDialog sender, ContentDialogButtonClickEventArgs args)
         {
+            _canClose = true;
+            this.Hide();
         }
     }
 }
