0a2aadb07be4dd86aa78e4f8f61fcea462b00e46
Damien <damientohin@gmail.com>
2018-04-24 10:45:04 +0100

    Added basic interfaces

---
+++ NativeCustomDialogs/NativeCustomDialogs.UWP/CallDialog.cs
@@ -0,0 +1,15 @@
+﻿using NativeCustomDialogs.UWP;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+[assembly: Xamarin.Forms.Dependency(
+   typeof(CallDialog))]
+namespace NativeCustomDialogs.UWP
+{
+    public class CallDialog
+    {
+    }
+}
---
+++ NativeCustomDialogs/NativeCustomDialogs/ICallDialog.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NativeCustomDialogs
+{
+    public interface ICallDialog
+    {
+        Task CallDialog(object viewModel);
+    }
+}
